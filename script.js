23ad69b4673007fab4623fc3f00b35e683b85081
Sam Gray <sgray@thinkingjuice.co.uk>
2020-11-30 18:23:59 +0000

    update

--- script.js
+++ script.js
@@ -15,270 +15,61 @@ var padding = {
 //randomNumbers = getRandomNumbers();
 //http://osric.com/bingo-card-generator/?title=HTML+and+CSS+BINGO!&words=padding%2Cfont-family%2Ccolor%2Cfont-weight%2Cfont-size%2Cbackground-color%2Cnesting%2Cbottom%2Csans-serif%2Cperiod%2Cpound+sign%2C%EF%B9%A4body%EF%B9%A5%2C%EF%B9%A4ul%EF%B9%A5%2C%EF%B9%A4h1%EF%B9%A5%2Cmargin%2C%3C++%3E%2C{+}%2C%EF%B9%A4p%EF%B9%A5%2C%EF%B9%A4!DOCTYPE+html%EF%B9%A5%2C%EF%B9%A4head%EF%B9%A5%2Ccolon%2C%EF%B9%A4style%EF%B9%A5%2C.html%2CHTML%2CCSS%2CJavaScript%2Cborder&freespace=true&freespaceValue=Web+Design+Master&freespaceRandom=false&width=5&height=5&number=35#results
 var data = [{
-    "label": "Adam Laker",
+    "label": "???",
     "value": 1,
-    "question": "Adam Laker"
-  }, // padding
-  {
-    "label": "Aimee Haines",
-    "value": 2,
-    "question": "Aimee Haines"
+    "question": "Tell an embarrassing Christmas story."
   },
   {
-    "label": "Alex Anderson",
-    "value": 3,
-    "question": "Alex Anderson"
-  },
-  {
-    "label": "Becki Woods",
-    "value": 3,
-    "question": "Becki Woods"
-  },
-  {
-    "label": "Ben Brown",
-    "value": 4,
-    "question": "Ben Brown"
-  },
-  {
-    "label": "Ben Trill",
-    "value": 5,
-    "question": "Ben Trill"
-  },
-  {
-    "label": "Beth Forsyth",
-    "value": 6,
-    "question": "Beth Forsyth"
-  },
-  {
-    "label": "Beth Forsyth",
-    "value": 7,
-    "question": "Beth Forsyth"
-  },
-  {
-    "label": "Beth Griffiths",
-    "value": 8,
-    "question": "Beth Griffiths"
-  },
-  {
-    "label": "Beth Morrison",
-    "value": 9,
-    "question": "Beth Morrion"
-  },
-  {
-    "label": "Calum Butchart",
-    "value": 10,
-    "question": "Calum Butchart"
-  },
-  {
-    "label": "Charlotte Williams",
-    "value": 11,
-    "question": "Charlotte Williams"
-  },
-  {
-    "label": "Courtney Johnson",
-    "value": 12,
-    "question": "Courtney Johnson"
-  },
-  {
-    "label": "Dan Morgan",
-    "value": 13,
-    "question": "Dan Morgan"
-  },
-  {
-    "label": "Elysha Westcott",
-    "value": 14,
-    "question": "Elysha Westcott"
-  },
-  {
-    "label": "Emily Coxx",
-    "value": 15,
-    "question": "Emily Coxx"
-  },
-  {
-    "label": "Fiona Treble",
-    "value": 16,
-    "question": "Fiona Treble"
-  },
-  {
-    "label": "Gabby Arthur",
-    "value": 17,
-    "question": "Gabby Arthur"
-  },
-  {
-    "label": "Gary Brookman",
-    "value": 18,
-    "question": "Gary Brookman"
-  },
-  {
-    "label": "Gemma Hartwll",
-    "value": 19,
-    "question": "Gemma Hartwll"
-  },
-  {
-    "label": "George Davis",
-    "value": 20,
-    "question": "George Davis"
-  },
-  {
-    "label": "Hannah Wright",
-    "value": 21,
-    "question": "Hannah Wright"
-  },
-  {
-    "label": "Harrison Hopton",
-    "value": 22,
-    "question": "Harrison Hopton"
-  },
-  {
-    "label": "Harry Finn",
-    "value": 23,
-    "question": "Harry Finn"
-  },
-  {
-    "label": "Henry Hayhurst",
-    "value": 24,
-    "question": "Harry Hayhurst"
-  },
-  {
-    "label": "Hollie Climie",
-    "value": 25,
-    "question": "Hollie Climie"
-  },
-  {
-    "label": "Holly Francis",
-    "value": 26,
-    "question": "Holly Francis"
-  },
-  {
-    "label": "Jack Murrell",
-    "value": 27,
-    "question": "Jack Murrell"
-  },
-  {
-    "label": "James Mallorie",
-    "value": 28,
-    "question": "James Mallorie"
-  },
-  {
-    "label": "Jess Nash",
-    "value": 29,
-    "question": "Jess Nash"
-  },
-  {
-    "label": "Jez Emery",
-    "value": 30,
-    "question": "Jez Emery"
-  },
-  {
-    "label": "Joe Worthington",
-    "value": 31,
-    "question": "Joe Worthington"
-  },
-  {
-    "label": "Joe Lewis",
-    "value": 32,
-    "question": "Joe Lewis"
-  },
-  {
-    "label": "Katie McGowan",
-    "value": 33,
-    "question": "Katie McGowan"
-  },
-  {
-    "label": "Keith McGregor",
-    "value": 34,
-    "question": "Keith McGregor"
-  },
-  {
-    "label": "Krista Bune",
-    "value": 35,
-    "question": "Krista Bune"
-  },
-  {
-    "label": "Laura Singleton",
-    "value": 36,
-    "question": "Laura Singleton"
-  },
-  {
-    "label": "Lewis Bullock",
-    "value": 37,
-    "question": "Lewis Bullock"
-  },
-  {
-    "label": "Lucy Kenny",
-    "value": 38,
-    "question": "Lucy Kenny"
-  },
-  {
-    "label": "Luke Bonner",
-    "value": 39,
-    "question": "Luke Bonner"
-  },
-  {
-    "label": "Meg Davis",
-    "value": 40,
-    "question": "Meg Davis"
-  },
-  {
-    "label": "Neil Dennis",
-    "value": 41,
-    "question": "Neil Dennis"
-  },
-  {
-    "label": "Nicky Clark",
-    "value": 42,
-    "question": "Nicky Clark"
-  },
-  {
-    "label": "Nicky Smith",
-    "value": 42,
-    "question": "Nicky Smith"
-  },
-  {
-    "label": "Oledia Challen",
-    "value": 43,
-    "question": "Oledia Challen"
-  },
-  {
-    "label": "Sam Gray",
-    "value": 44,
-    "question": "Sam Gray"
-  },
-  {
-    "label": "Sam Leonard",
-    "value": 45,
-    "question": "Sam Leonard"
-  },
-  {
-    "label": "Sam Liang",
-    "value": 46,
-    "question": "Sam Liang"
-  },
-  {
-    "label": "Stephan Flagg",
-    "value": 47,
-    "question": "Stephan Flagg"
-  },
-  {
-    "label": "Stewart Austin",
-    "value": 48,
-    "question": "Stewart Austin"
-  },
-  {
-    "label": "Terry Davis",
-    "value": 49,
-    "question": "Terry Davis"
-  },
-  {
-    "label": "Tim Redman",
-    "value": 50,
-    "question": "Tim Redman"
-  },
-  {
-    "label": "Xander Ettling",
-    "value": 51,
-    "question": "Xander Ettling"
-  },
+      "label": "???",
+      "value": 2,
+      "question": "Speech or Shot."
+    },
+    {
+        "label": "???",,
+        "value": 3,
+        "question": "You are an Elf of the Shelf and can only move when no one is watching for the rest of the game."
+      },
+      {
+          "label": "???",
+          "value": 4,
+          "question": "Send a text to a random number (that we’ll pick) in your phone book."
+        },
+        {
+            "label": "???",
+            "value": 5,
+            "question": "Go on Facebook Live and read the back of a shampoo bottle."
+          },
+          {
+              "label": "???",
+              "value": 6,
+              "question": "Call a random number and sing Happy Birthday."
+            },
+            {
+                "label": "???",
+                "value": 7,
+                "question": "Silently do the macarena."
+              },
+              {
+                  "label": "???",
+                  "value": 8,
+                  "question": "Sing a song chosen by the group while eating spoonfuls of peanut butter/marmite."
+                },
+                {
+                    "label": "???",
+                    "value": 9,
+                    "question": "Lip Sync Battle (2 players required) "
+                  },
+                  {
+                      "label": "???",
+                      "value": 10,
+                      "question": "Minus 10 points."
+                    },
+                    {
+                        "label": "???",
+                        "value": 9,
+                        "question": "???",
+                      },
+
 ];
 var svg = d3.select('#chart')
   .append("svg")
